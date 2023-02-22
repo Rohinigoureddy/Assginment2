6e169fcfeb22e73ee81d5c8898993516b6f065d2
rohinigoureddy <rxg24130@ucmo.edu>
2023-02-21 20:14:51 -0600

    created file

--- boto_list.py
+++ boto_list.py
@@ -1,6 +1,10 @@
 import boto3
+import cred
+
+s3 = cred.resource('s3')
 s3_resource = boto3.resource('s3')
 s3_resource.create_bucket(Bucket="aws-rohini")
 
+f = open('bucket_names.txt')
 for bucket in s3_resource.buckets.all():
-    print(bucket.name)
+    f.write(bucket['Name'] + '\n')
---
+++ cred.py
@@ -0,0 +1,3 @@
+import boto3
+
+session = boto3.Session( aws_access_key_id='Rohini14', aws_secret_access_key='Medha@16')
